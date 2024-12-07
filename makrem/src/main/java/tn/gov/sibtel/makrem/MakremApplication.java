33a5dc09bc54fe24d6978aeafd4fdcf2d7bcf72f
makremZitoun23 <148913206+makremZitoun23@users.noreply.github.com>
2024-12-07 08:47:03 +0000

    config server remote and local

---
+++ makrem/src/main/java/tn/gov/sibtel/makrem/MakremApplication.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.makrem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakremApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakremApplication.class, args);
+	}
+
+}
---
+++ makrem/src/test/java/tn/gov/sibtel/makrem/MakremApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.makrem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakremApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
