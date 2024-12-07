064d3610f9446915f0e6acbe644096597d31bea4
makremZitoun23 <148913206+makremZitoun23@users.noreply.github.com>
2024-12-07 12:06:37 +0000

    eureka server

---
+++ slaheddine/src/main/java/tn/gov/sibtel/slaheddine/SlaheddineApplication.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.slaheddine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaheddineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlaheddineApplication.class, args);
+	}
+
+}
---
+++ slaheddine/src/test/java/tn/gov/sibtel/slaheddine/SlaheddineApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.slaheddine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaheddineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
